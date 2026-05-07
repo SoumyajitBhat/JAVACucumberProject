4bf589e827b573d66ad315340a2ee9bf05d082c2
Soumyajit Bhattacharjee <soumyajitbhattacharjee@CKMAC389.local>
2026-05-07 17:23:06 +0530

    Update DriverManager to run Chrome in headless mode for CI

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
 
 public class DriverManager {
@@ -16,7 +17,15 @@ public class DriverManager {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments(
+                "--headless=new",
+                "--disable-gpu",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--window-size=1920,1080"
+            );
+            driver = new ChromeDriver(options);
             driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         }
         return driver;
